f86fcd1040f656fff9fc7ac8680467f654eba7b2
Uriel <CarrilloUriel81@gmail.com>
2023-11-01 14:10:40 -0700

    Added Post and Retrieve Notes

--- Develop/Assets/server.js
+++ Develop/Assets/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const api = require('<!--Routes to api -->')
 const app = express();
 const PORT = 3001;
-const { v4: uuidv4 } = require('uuid');
+//const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
@@ -13,14 +12,33 @@ app.use(express.static('public'));
 
 let notes = [];
 
+// Reads Notes
+app.get('/api/notes', (req, res) =>  {
+    const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+    res.json(notesData.notes);
+});
 
-app.get('/', (req, res) =>  
-    res.sendFile(path.join(__dirname, '/Develop/index.html'))
-);
 
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/notes.html'))
-);
+// Creates New Notes
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+
+  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+  notesData.notes.push(newNote);
+
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notesData));
+
+  res.json(newNote);
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/index.html'));
+});
 
 app.listen(PORT, () =>
     console.log(`App Listening at http://localhost:${PORT}`)
