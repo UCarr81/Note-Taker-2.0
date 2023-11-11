71915e4af15d79b297f7f55080d4708ab9f6776e
Uriel <CarrilloUriel81@gmail.com>
2023-11-10 16:34:33 -0800

    Delete Post upon click works now

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -18,10 +18,38 @@ app.post('/api/notes', (req, res) => {
   const note = req.body;
   console.log(notes);
   notes.push(note);
-  fs.writeFileSync(path.join(__dirnamer, '../db/db.json'). JSON.stringify(notes));
+  fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
   res.json(notes);
 });
 
+//Delete
 
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const noteIndex = notes.findIndex(note => note.id === noteId);
 
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+    res.json(notes);
+  } 
+});
 }
+
+
+//app.delete('/api/notes/:id', function (req, res) {
+//  const id = req.body.id;
+//  console.log('Troubleshooting notes with id function? Working?')
+//  for (i = 0; i < notes.length; i++) {
+//    if (notes[i].id === parseInt(id)) {
+//      notes.splice(i, 1);
+//      break;
+//    }
+//  }
+//
+//  fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+//  res.json(notes);
+//});
+//
+//}
