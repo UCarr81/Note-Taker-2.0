85837cfd7e6d5a76debb04837791b70be757080a
Uriel <CarrilloUriel81@gmail.com>
2023-11-10 16:16:20 -0800

    App Posts Notes officially!

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,15 +4,23 @@ const fs = require('fs');
 const uuid = require('../helpers/uuid');
 
 
-
+//Routing
 module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
       res.json(notes);
 });
 
 
+//post
 
-
+app.post('/api/notes', (req, res) => {
+  req.body.id = uuid();
+  const note = req.body;
+  console.log(notes);
+  notes.push(note);
+  fs.writeFileSync(path.join(__dirnamer, '../db/db.json'). JSON.stringify(notes));
+  res.json(notes);
+});
 
 
 
